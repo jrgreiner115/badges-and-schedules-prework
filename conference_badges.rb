9ebcc3c455e70978bba4ef9835e16b919461d556
Joe Greiner <jrgreiner115@gmail.com>
2018-03-23 14:05:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+badges = []
+attendees.each {|name| badges << "Hello, my name is #{name}."}
+badges
+end
+
+def assign_rooms(speakers)
+  badges = []
+attendees.each {|name| badges << "Hello, my name is #{name}."}
+badges
+end
+
+def printer
+end
